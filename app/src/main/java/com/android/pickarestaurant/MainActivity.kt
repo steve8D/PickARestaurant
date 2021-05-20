931d58a8320bf187bb629bb799369bb67fbdcdce
Danny Ngo <70412140+steve8D@users.noreply.github.com>
2021-05-19 17:45:27 -0700

    Initial commit

---
+++ app/src/main/java/com/android/pickarestaurant/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.android.pickarestaurant
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.android.pickarestaurant.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/android/pickarestaurant/TitleFragment.kt
@@ -0,0 +1,18 @@
+package com.android.pickarestaurant
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.android.pickarestaurant.databinding.FragmentTitleBinding
+
+class TitleFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        val binding: FragmentTitleBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_title, container, false)
+        return binding.root
+    }
+}
